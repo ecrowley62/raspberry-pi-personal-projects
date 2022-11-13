6540f4d6702a555725a63dd7c91afc7582f06209
Eric Crowley <eecmtf@gmail.com>
2022-11-13 16:00:52 -0700

    Initial script for testing interactions with the display

---
+++ functionality_exploration/write_text_to_display.py
@@ -0,0 +1,15 @@
+from RPLCD.i2c import CharLCD
+
+I2C_ADDRESS = '0x3f'
+I2C_PORT_EXPANDER = 'PCF8573'
+
+
+def main() -> None:
+
+    # Create an interface for the LCD screen then
+    # write a string to it
+    lcd = CharLCD(I2C_PORT_EXPANDER, I2C_ADDRESS)
+    lcd.write_string('Hello Govena')
+
+if __name__ == '__main__':
+    main()

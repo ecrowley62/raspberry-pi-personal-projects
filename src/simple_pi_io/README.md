ea377476f5f1776e7ba40d062412e4e9648adecf
Eric Crowley <eecmtf@gmail.com>
2022-11-13 13:09:42 -0700

    start of a library for abstracting GPIO operations with the Raspberry PI using RPi.GPIO, which itself is already an abstraction. The idea behind the raspberry pip is quick proto-typing, so Im thinking this GPIO interface should be as simple/abstract as possible

---
+++ src/simple_pi_io/README.md
@@ -0,0 +1,3 @@
+# Overview
+Module gives a simple interface for interacting with a Raspberry Pi's GPIO
+pins using the RPi.GPIO module
---
+++ src/simple_pi_io/interfaces.py
@@ -0,0 +1,69 @@
+from typing import Dict, List, Tuple, Optional, Union
+import RPi.GPIO as GPIO
+
+
+class GpioOutputChannel:
+
+    def _setup_channel(self) -> None:
+        GPIO.setup(self.number, GPIO.OUT, initial=self.initial_state)
+
+    def __init__(self, 
+                 number: int,
+                 initial_state: int = GPIO.LOW
+                 ) -> None:
+        self.number = number
+        self.initial_state = initial_state
+        self._setup_channel()
+
+    def turn_on(self) -> bool:
+        GPIO.output(self.number, GPIO.HIGH)
+        return True
+
+    def turn_off(self) -> bool:
+        GPIO.output(self.number, GPIO.LOW)
+        return True
+
+    def cleanup(self) -> bool:
+        GPIO.cleanup(self.number)
+        return True
+
+
+class SimpleGpio:
+
+    DEFAULT_MODE = GPIO.BOARD
+    SET_WARNINGS = False
+
+    def __init__(self,
+                 gpio_mode: int = DEFAULT_MODE,
+                 set_warnings: bool = SET_WARNINGS
+                 ) -> None:
+        self.gpio_mode = gpio_mode
+        self.set_warnings = set_warnings
+        self.channels = {}
+
+    def __enter__(self) -> None:
+        GPIO.setmode(self.gpio_mode)
+        GPIO.setwarnings(self.set_warnings)
+
+    def __exit__(self) -> None:
+        channel_pins = [pin for pin in self.channels.keys()]
+        GPIO.cleanup(channel_pins)
+
+    def get_channel(self, pin_number: int) -> GpioOutputChannel:
+        try:
+            channel = self.channels[pin_number]
+        except KeyError:
+            channel = GpioOutputChannel(pin_number)
+            self.channels[pin_number] = channel
+        finally:
+            return channel
+
+    def turn_on_output(self, pin_number: int) -> bool:
+        channel = self.get_channel(pin_number)
+        result = channel.turn_on()
+        return result
+
+    def turn_off_output(self, pin_number: int) -> None:
+        channel = self.get_channel(pin_number)
+        result = channel.turn_off()
+        return result

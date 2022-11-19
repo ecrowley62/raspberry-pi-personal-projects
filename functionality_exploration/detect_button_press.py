8b146b493a3aea38c3d3f108e37555d3c4f7df6d
Eric Crowley <eecmtf@gmail.com>
2022-11-19 12:37:40 -0700

    plus test script for testing registering a button push

--- functionality_exploration/detect_button_press.py
+++ functionality_exploration/detect_button_press.py
@@ -1,2 +1,25 @@
+from tkinter import BUTT
 from typing import Dict, List, Tuple
+import RPi.GPIO as GPIO
 
+BUTTON_PIN = 35
+
+def setup() -> None:
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+def register_button_push_input() -> None:
+    if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
+        print("Button has been pushed")
+    else:
+        print("Button has NOT been pushed")
+    print('\n')
+
+def main() -> None:
+    setup()
+    while True:
+        register_button_push_input()
+
+if __name__ == '__main__':
+    main()
